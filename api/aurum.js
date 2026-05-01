1d00fcb580b55f53cfe61029e5b0d788faefe648
getaurum <getaurum2026@gmail.com>
2026-05-01 20:56:17 +0200

    Update aurum.js

--- api/aurum.js
+++ api/aurum.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
       });
       const data = await response.json();
 const cleaned = removeCiteTags(data);
-const reprocessed = JSON.parse(JSON.stringify(cleaned).replace(/<cite[^>]*>(.*?)<\/cite>/gs, '$1'));
+const reprocessed = JSON.parse(JSON.stringify(cleaned).replace(/<cite[^>]*>[\s\S]*?<\/cite>/g, (m) => m.replace(/<cite[^>]*>/g, '').replace(/<\/cite>/g, '')));
 return res.status(response.status).json(reprocessed);
     } catch (error) {
       return res.status(500).json({ error: 'Anthropic API error' });
